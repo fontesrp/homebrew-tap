0538912b8ae908b26a358e03b484bde1b5ea59de
Rodrigo Prado Fontes <fontesr2@gmail.com>
2022-06-01 23:33:15 -0300

    Update kscript.rb

--- kscript.rb
+++ kscript.rb
@@ -1,14 +1,14 @@
 class Kscript < Formula
   desc "kscript"
   homepage "https://github.com/holgerbrandl/kscript"
-  url "https://github.com/holgerbrandl/kscript/releases/download/v4.0.3/kscript-4.0.3-bin.zip"
-  sha256 "e92b000ea13dd48c49329269b025d59c3e0ff507fe3a357fb5af401773b1cdbb"
+  url "https://github.com/holgerbrandl/kscript/releases/download/v3.1.0/kscript-3.1.0-bin.zip"
+  sha256 "e96da7ffe448ec7c993e821454946cd5f41ba0d6e371c56efb56d0191bc68372"
 
   depends_on "kotlin"
 
   def install
     libexec.install Dir["*"]
-    inreplace "#{libexec}/bin/kscript", /^JAR_PATH=.*/, "JAR_PATH=#{libexec}/bin/kscript.jar"
+    inreplace "#{libexec}/bin/kscript", /^jarPath=.*/, "jarPath=#{libexec}/bin/kscript.jar"
     bin.install_symlink "#{libexec}/bin/kscript"
   end
 end
